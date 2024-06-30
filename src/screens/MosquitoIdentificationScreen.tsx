2bb46c0ab5786f2ea97dc43841bf7827f2fb5654
Aryaman Shodhan <ashodha1@jhu.edu>
2024-06-30 01:10:55 -0400

    Implement image capture

--- src/screens/MosquitoIdentificationScreen.tsx
+++ src/screens/MosquitoIdentificationScreen.tsx
@@ -1,6 +1,6 @@
-import React, {useState, useEffect} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import {Camera} from 'react-native-vision-camera';
+import React, {useState, useEffect, useRef, useCallback} from 'react';
+import {View, Text, StyleSheet, Pressable} from 'react-native';
+import {Camera, useCameraDevice} from 'react-native-vision-camera';
 
 import {COLORS} from '../assets/constants/theme';
 
@@ -8,6 +8,8 @@ const MosquitoIdentificationScreen = () => {
   const [cameraPermission, setCameraPermission] = useState(
     Camera.getCameraPermissionStatus(),
   );
+  const cameraRef = useRef<Camera>(null);
+  const device = useCameraDevice('back')!;
 
   useEffect(() => {
     const verifyCameraPermission = async () => {
@@ -23,6 +25,13 @@ const MosquitoIdentificationScreen = () => {
     verifyCameraPermission();
   }, []);
 
+  const captureImageHandler = useCallback(async () => {
+    if (cameraRef.current) {
+      const image = await cameraRef.current.takePhoto();
+      console.log(image);
+    }
+  }, [cameraRef]);
+
   if (cameraPermission !== 'granted') {
     return (
       <View>
@@ -32,8 +41,23 @@ const MosquitoIdentificationScreen = () => {
   }
 
   return (
-    <View>
-      <Text>Granted Permission!!!!!!!</Text>
+    <View style={styles.rootContainer}>
+      <Camera
+        style={StyleSheet.absoluteFill}
+        photo={true}
+        device={device}
+        isActive={true}
+        ref={cameraRef}
+      />
+      <Pressable
+        style={({pressed}) =>
+          pressed
+            ? [styles.captureButton, styles.captureButtonPressed]
+            : styles.captureButton
+        }
+        onPress={captureImageHandler}>
+        <View style={styles.staticInnerCircle} />
+      </Pressable>
     </View>
   );
 };
@@ -44,5 +68,27 @@ const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
     backgroundColor: COLORS.white,
+  },
+  captureButton: {
+    width: 80,
+    height: 80,
+    borderWidth: 7,
+    borderColor: COLORS.white,
+    borderRadius: 40,
+    position: 'absolute',
+    bottom: 40,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  captureButtonPressed: {
+    opacity: 0.25,
+  },
+  staticInnerCircle: {
+    width: 50,
+    height: 50,
+    backgroundColor: COLORS.white,
+    borderRadius: 25,
+    position: 'absolute',
   },
 });
