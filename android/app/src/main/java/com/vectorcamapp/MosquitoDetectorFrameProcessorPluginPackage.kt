63f467795dfac3fe915d8cf845d1266cdc96c072
Aryaman Shodhan <ashodha1@jhu.edu>
2024-07-18 01:45:38 -0400

    Add yolo model to app

--- android/app/src/main/java/com/vectorcamapp/MosquitoDetectorFrameProcessorPluginPackage.kt
+++ android/app/src/main/java/com/vectorcamapp/MosquitoDetectorFrameProcessorPluginPackage.kt
@@ -1,8 +1,10 @@
 package com.vectorcamapp
 
+import android.view.View
 import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ReactShadowNode
 import com.facebook.react.uimanager.ViewManager
 import com.mrousavy.camera.frameprocessors.FrameProcessorPlugin
 import com.mrousavy.camera.frameprocessors.FrameProcessorPluginRegistry
@@ -15,12 +17,12 @@ class MosquitoDetectorFrameProcessorPluginPackage: ReactPackage {
             }
         }
     }
+    override fun createViewManagers(
+            reactContext: ReactApplicationContext
+    ): MutableList<ViewManager<View, ReactShadowNode<*>>> = mutableListOf()
 
     override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
-        return emptyList()
-    }
-
-    override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
-        return emptyList()
+        val modules = mutableListOf<NativeModule>()
+        return modules
     }
 }
